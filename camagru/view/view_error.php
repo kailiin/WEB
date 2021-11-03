fca4253ca3bb2621e009a477a4c082e0def51fb5
kai lin <41614139+kailiin@users.noreply.github.com>
2021-11-03 20:42:16 +0100

    Delete camagru directory

--- camagru/view/view_error.php
+++
@@ -1,6 +0,0 @@
-<?php $titre = 'Error' ?>
-<?php ob_start(); ?>
-<p>ERROR : <?php echo $error ?></p>
-<?php $content = ob_get_clean(); ?>
-
-<?php require 'view/layout.php'; ?>
--- camagru/view/view_home.php
+++
@@ -1,9 +0,0 @@
-<?php $titre = 'Homepage'; ?>
-
-<?php ob_start(); ?>
-
-<h1>Homepage</h1>
-
-<?php $content = ob_get_clean(); ?>
-
-<?php require 'view/layout.php' ?>
